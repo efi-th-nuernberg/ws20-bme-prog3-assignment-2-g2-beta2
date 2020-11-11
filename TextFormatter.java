ca616a48e441fd1421ef5fcfb37af724c6f0fd80
repl.it user <replituser@example.com>
2020-11-11 12:58:49 +0000

    3

--- TextFormatter.java
+++ TextFormatter.java
@@ -14,7 +14,9 @@ class TextFormatter {
 
   public static void main(String[] args) {
     TextFormatter formatter = new TextFormatter(30);
-    formatter.print(text);
+    //formatter.print(text);
+    formatter.printRightAlign(text);
+ 
     
   }
 
@@ -28,17 +30,43 @@ class TextFormatter {
   public void print(String aText) {
     String[] words = aText.split(" ");
     int characterCounter = 0;
-	  for(int i = 0; i < words.length; i++) {
-      int wordLength = words[i].length();
-	    if(characterCounter + wordLength <= maxLength) {
-	      characterCounter += wordLength + 1;
-	    	System.out.print(words[i] + " ");
-	    }else{
-	    	System.out.print("\n" + words[i] + " ");
-        characterCounter = wordLength + 1;
+      for(int i = 0; i < words.length; i++) {
+        int wordLength = words[i].length();
+          if(characterCounter + wordLength <= maxLength) {
+            characterCounter += wordLength + 1;
+            System.out.print(words[i] + " ");
+          }else{
+            System.out.print("\n" + words[i] + " ");
+            characterCounter = wordLength + 1;
+          }
       }
-    }
       System.out.println();
   }
+ 
+
+ public void printRightAlign(String aText) {
+    String[] words = aText.split(" ");
+    String line = "";
+    int characterCounter = 0;
+      for (int i = 0; i < words.length; i++) {
+        int wordLength = words[i].length();
+          if (characterCounter + wordLength <= maxLength) {
+            characterCounter += wordLength + 1;
+            line += " " + words[i];
+          } else {
+            while (line.length() < maxLength) {
+              line = " " + line;
+            }
+            System.out.print(line + "\n");
+            characterCounter = wordLength + 1;
+            line = " " + words[i];
+          }
+        }
+          while(line.length() < maxLength){
+          line = " " + line;
+          }
+        System.out.println(line);
+      }
+
 
 }
